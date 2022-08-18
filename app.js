7fdffb4d5e8a8cee98a28f932476ae8b79a1b4f3
Sanjith <sanjithraghav45@gmail.com>
2022-08-18 17:38:21 +0530

    backend done

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app=express();
+app.use(bodyParser.urlencoded({extended:true}));
+app.set('view engine', 'ejs');
+
+var lists=[];
+app.listen(3000);
+app.get('/', (req,res)=>{
+    var date=new Date();
+    var options={'weekday':'long','day':'numeric','month':'long'};
+    var day=date.toLocaleDateString('en-US',options);
+    res.render('list',{DAY:day,items:lists});
+});
+app.post('/',(req,res)=>{
+    lists.push(req.body.new);
+    res.redirect('/');
+})
+
+
+

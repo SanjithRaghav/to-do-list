27eb9db11ebf8c12257378717be2c73a6e1c696c
Sanjith <sanjithraghav45@gmail.com>
2022-08-18 20:16:39 +0530

    style added

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const app=express();
 app.use(bodyParser.urlencoded({extended:true}));
 app.set('view engine', 'ejs');
-
+app.use(express.static('public'))
 var lists=[];
 app.listen(3000);
 app.get('/', (req,res)=>{
@@ -15,7 +15,8 @@ app.get('/', (req,res)=>{
 app.post('/',(req,res)=>{
     lists.push(req.body.new);
     res.redirect('/');
-})
+});
+
 
 
 
